a9acb8d55480fc32fa78b7f7b7802a13a5c6d17c
Hashim Warren <hashimwarren@gmail.com>
2025-04-25 14:50:28 -0400

    Add old blog posts

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,19 +3,23 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { allAuthors } from 'contentlayer/generated'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
+  // Get the default author or the first author in the list
+  const defaultAuthor = allAuthors.find((author) => author.slug === 'default') || allAuthors[0]
+
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
+            {siteMetadata.description}
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+            Edited by {defaultAuthor?.name}
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
