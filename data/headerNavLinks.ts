97db1f93dfd51f8038e9050a83507c2ab6a3b00c
Hashim Warren <hashimwarren@gmail.com>
2025-04-25 15:12:01 -0400

    Hide projects in nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,8 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  // TODO: Add custom projects before re-enabling the Projects link
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
