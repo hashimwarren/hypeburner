c84516a61f0f012673c3d74cf5235fa79e45710c
Hashim Warren <hashimwarren@gmail.com>
2026-02-18 16:31:32 -0500

    Fix Payload serverFunction typing for Vercel build

--- app/(payload)/serverFunction.ts
+++ app/(payload)/serverFunction.ts
@@ -2,7 +2,14 @@
 
 import { handleServerFunctions } from '@payloadcms/next/layouts'
 import type { ServerFunctionClient } from 'payload'
+import configPromise from '../../payload.config'
+
+import { importMap } from './cms/importMap'
 
 export const serverFunction: ServerFunctionClient = async (args) => {
-  return handleServerFunctions(args)
+  return handleServerFunctions({
+    ...args,
+    config: configPromise,
+    importMap,
+  })
 }
